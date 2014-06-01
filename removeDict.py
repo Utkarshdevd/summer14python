2c06e488a86fedd3556d5eab78530ac2c19b88a0
Utkarshdevd <utkarshdvd@gmail.com>
2014-06-01 22:56:23 +0530

    updates dictionary, but still has discrepancies

---
+++ removeDict.py
@@ -0,0 +1,52 @@
+import sys
+import nameGen
+
+'''Get Parameter File data'''
+parameterFile = sys.argv[-1]
+
+paramF = open(parameterFile, 'r')
+header = paramF.readline().strip()
+path = paramF.readline().strip().split(" ")[2]
+startNo = int(paramF.readline().strip().split(" ")[2])
+endNo = int(paramF.readline().strip().split(" ")[2])
+ext = paramF.readline().strip().split(" ")[2]
+baseName = paramF.readline().strip().split(" ")[2]
+res_Path = paramF.readline().strip().split(" ")[2]
+res_BaseName = paramF.readline().strip().split(" ")[2]
+res_Extension = paramF.readline().strip().split(" ")[2]
+
+'''Initalize Dictionary'''
+dictionaryFile = sys.argv[-2]
+# Get vocabulary
+with open(dictionaryFile, "r") as myfile:
+		vocab = myfile.read()
+vocab = vocab.split()
+
+'''Weed out words found in vocabulary and add new words to it'''
+res = []
+'''
+Attempt idiomatic version, this one cannot update vocab at the same time, hence has redundant words
+but is faster than the other approach
+res = [word for word in data if not word in vocab]
+'''
+
+flag = True
+for currentNo in range(startNo, endNo+1):
+	# Get file name
+	fileName = nameGen.getVar(res_Path, res_BaseName, currentNo, res_Extension)
+	# Get file data
+	with open(fileName, "r") as myfile:
+	    data = myfile.read()
+
+	data = data.split()
+	for word in data:
+		for v in vocab:
+			if v == word:
+				flag = False
+
+		if flag:
+			vocab.append(word)
+			res2.append(word)
+		flag = True
+
+print len(res2)
